743c6e2f9ab591556d92914bdcd204cfb0f46bbb
KenLee <ken4000kl@gmail.com>
2020-07-06 16:06:22 +0800

    Add syntax file.

--- main.py
+++ main.py
@@ -8,4 +8,5 @@ import sublime_plugin
 
 class ExampleCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		# self.view.insert(edit, 0, "Hello, World!")
+		print("asdssda")
